79db931824cdcab79011323207c796aea023ec26
joaquinnieva <joacoo.nieva@gmail.com>
2022-03-08 18:41:20 -0300

    loader component added

---
+++ components/Loader.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Loader() {
+  return (
+    <div className="w-12 h-12 border-4 border-solid rounded-full border-zinc-500 animate-spin border-t-transparent"></div>
+  );
+}
+
+export default Loader;
