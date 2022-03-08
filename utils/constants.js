dfaa2e00cbd88b8eef7ae9add9b1e3a7e44a7253
joaquinnieva <joacoo.nieva@gmail.com>
2022-03-08 18:42:41 -0300

    data created

---
+++ utils/constants.js
@@ -0,0 +1,16 @@
+export const INFO_TITLE1 = 'Before they sold out';
+export const INFO_TITLE2 = 'readymade gluten';
+export const INFO_SUBTITLE =
+  'Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.';
+export const INFO_BUTTON_PRY = 'View more';
+export const INFO_BUTTON_SEC = 'Register';
+export const LOGIN_TITLE = 'Login';
+export const LOGIN_SUBTITLE = 'Stay updated on your professional world';
+export const LOGIN_BUTON = 'Login';
+export const LOGIN_QUESTION = 'New to Librery?';
+export const LOGIN_LINK = 'Join now';
+export const REGISTER_TITLE = 'Register';
+export const REGISTER_SUBTITLE = 'Stay updated on your professional world';
+export const REGISTER_BUTTON = 'Register';
+export const REGISTER_QUESTION = 'Have you account?';
+export const REGISTER_LINK = 'Login';
